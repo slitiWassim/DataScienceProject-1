7017a1f99e0b3a494a0282e18f88d2cd2da0b94d
Wassim <wassimsliti1998@gmail.com>
2025-04-01 10:05:32 +0100

    Model Training pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.DataScience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.DataScience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.DataScience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.DataScience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.DataScience import logger
 
 
@@ -34,6 +35,17 @@ if __name__ == '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
         obj.initiate_data_transformation()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    STAGE_NAME = "Model Trainer Stage"
+
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.initiate_model_training()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
